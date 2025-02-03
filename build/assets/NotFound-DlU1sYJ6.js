b59e2d1d90984b10472a89e8bc46c4ac10d462d7
pro_dip <prodip.sarker@sslwireless.com>
2025-02-03 15:52:55 +0600

    added light and dark mode

--- build/assets/NotFound-DlU1sYJ6.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BO7hX5no.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
--- build/assets/Workplace-6sWLhS-4.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BO7hX5no.js";const t=()=>e.jsx("div",{className:"text-2xl",children:"Workplace"});export{t as default};
