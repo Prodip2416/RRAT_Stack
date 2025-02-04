2aeb052ce053b51f4125f04e0b1844f03b241a3c
pro_dip <prodip.sarker@sslwireless.com>
2025-02-04 10:44:17 +0600

    added redux RTK and use this on auth and ui theme

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  ENVIRONMENT: "dev", //prod/dev
+  ENVIRONMENT: "prod", //prod/dev
   BASE_URL: "https://api.example.com",
   BASE_PATH_DEV: "",
   BASE_PATH_PROD: "/RRAT_Stack/",
